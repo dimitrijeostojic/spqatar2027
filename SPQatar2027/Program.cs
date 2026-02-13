b9d87c2d7c3ea61a43ce346cb3092ec7f69328ce
Dimitrije Ostojic <dimitrijeo@invaryant.com>
2026-02-13 17:19:42 +0100

    remove deploy to azure

--- SPQatar2027/Program.cs
+++ SPQatar2027/Program.cs
@@ -38,9 +38,11 @@ builder.Services.AddSwaggerGenWithAuth(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-app.UseSwagger();
-app.UseSwaggerUI();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
